05b1db4d732d560a65819ca54e002cbc7bccd840
Erickype <erickype@hotmail.com>
2023-08-03 14:11:33 -0500

    Add rule method, still needs the database

--- rule/docs.go
+++ rule/docs.go
@@ -1,4 +1,4 @@
 // This package manages the rules used by the rule engine,
 // the rule engine to be used will be https://github.com/hyperjumptech/grule-rule-engine
 
-package rules
+package rule
--- rule/rule.go
+++ rule/rule.go
@@ -1,4 +1,9 @@
-package rules
+package rule
+
+import (
+	"context"
+	"fmt"
+)
 
 // Rule is a representation of the GRule json format, used only the basic format.
 // https://github.com/hyperjumptech/grule-rule-engine/blob/master/docs/en/GRL_JSON_en.md
@@ -8,4 +13,12 @@ type Rule struct {
 	Salience int64    `json:"salience"`
 	When     string   `json:"when"`
 	Then     []string `json:"then"`
+}
+
+// Add inserts a Rule in the database
+//
+//encore:api public method=POST path=/rule
+func Add(_ context.Context, rule *Rule) error {
+	fmt.Print(rule)
+	return nil
 }
