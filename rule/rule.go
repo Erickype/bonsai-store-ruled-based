0edfc92aeb9c6f3d18b083cdaf97861ed0739806
Erickype <erickype@hotmail.com>
2023-08-04 11:43:21 -0500

    Rule fields documentation

--- rule/rule.go
+++ rule/rule.go
@@ -7,11 +7,16 @@ import (
 // Rule is a representation of the GRule json format, used only the basic format.
 // https://github.com/hyperjumptech/grule-rule-engine/blob/master/docs/en/GRL_JSON_en.md
 type Rule struct {
-	Name     string   `json:"name"`
-	Desc     string   `json:"desc"`
-	Salience int64    `json:"salience"`
-	When     string   `json:"when"`
-	Then     []string `json:"then"`
+	// Name represents the rule name
+	Name string `json:"name"`
+	// Desc is the rule description
+	Desc string `json:"desc"`
+	// Salience represent the relevance of the rule
+	Salience int64 `json:"salience"`
+	// When are the conditions of the rule
+	When string `json:"when"`
+	// Then represents the action when the condition is true
+	Then []string `json:"then"`
 }
 
 // Add inserts a Rule in the database
