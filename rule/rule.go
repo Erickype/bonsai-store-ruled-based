000c94a6d05e2e367f01d53417b976b0b59350ff
Erickype <erickype@hotmail.com>
2023-08-05 13:40:56 -0500

    Update rule

--- rule/rule.go
+++ rule/rule.go
@@ -45,3 +45,14 @@ func GetById(ctx context.Context, id int) (*Rule, error) {
 	}
 	return rule, nil
 }
+
+// Update modifies the values of a rule by its ID
+//
+//encore:api public method=PUT path=/rule
+func Update(ctx context.Context, rule *Rule) error {
+	query := `UPDATE rule 
+			SET name = $1, "desc" = $2, salience = $3, "when" = $4, "then" = $5 
+			WHERE id = $6`
+	_, err := ruleDb.Exec(ctx, query, rule.Name, rule.Desc, rule.Salience, rule.When, rule.Then, rule.ID)
+	return err
+}
