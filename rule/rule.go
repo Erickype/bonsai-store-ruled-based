7a991f29824ed4d38ccc304215271140fc458cec
Erick Carrasco <erickype@hotmail.com>
2023-08-07 21:08:24 +0000

    Delete endpoint to remove a rule based on its ID

--- rule/rule.go
+++ rule/rule.go
@@ -54,7 +54,7 @@ func GetById(ctx context.Context, id int) (*Rule, error) {
 //
 //encore:api public method=PUT path=/rule
 func Update(ctx context.Context, rule *Rule) error {
-	query := `UPDATE rule 
+	query := `update rule 
 			SET name = $1, "desc" = $2, salience = $3, "when" = $4, "then" = $5 
 			WHERE id = $6`
 	result, err := ruleDb.Exec(ctx, query, rule.Name, rule.Desc, rule.Salience, rule.When, rule.Then, rule.ID)
@@ -65,6 +65,25 @@ func Update(ctx context.Context, rule *Rule) error {
 		return &errs.Error{
 			Code:    errs.NotFound,
 			Message: "rule not updated",
+		}
+	}
+	return nil
+}
+
+// Delete removes a rule from the database base on its ID
+//
+//encore:api public method=DELETE path=/rule/:id
+func Delete(ctx context.Context, id int) error {
+	query := `delete from rule
+			where id = $1`
+	result, err := ruleDb.Exec(ctx, query, id)
+	if err != nil {
+		return err
+	}
+	if result.RowsAffected() == 0 {
+		return &errs.Error{
+			Code:    errs.NotFound,
+			Message: "rule not deleted",
 		}
 	}
 	return nil
