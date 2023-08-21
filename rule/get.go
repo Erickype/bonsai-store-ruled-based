e54853d9630a76a7f2960dbc9c6659e9aee941ba
Erick Carrasco <erickype@hotmail.com>
2023-08-21 19:37:08 +0000

    Get in a distinct file

---
+++ rule/get.go
@@ -0,0 +1,47 @@
+package rule
+
+import "context"
+
+// ListResponse is the response of the Get method
+type ListResponse struct {
+	// Rules holds the list of rules
+	Rules []*Rule
+}
+
+// Get return the list of rules
+//
+//encore:api public method=GET path=/rule
+func Get(ctx context.Context) (*ListResponse, error) {
+	query := "select * from rule"
+	rows, err := ruleDb.Query(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+	response := &ListResponse{Rules: []*Rule{}}
+
+	for rows.Next() {
+		rule := &Rule{}
+		err = rows.Scan(&rule.ID, &rule.Name, &rule.Desc, &rule.Salience, &rule.When, &rule.Then)
+		if err != nil {
+			return nil, err
+		}
+		response.Rules = append(response.Rules, rule)
+	}
+
+	return response, nil
+}
+
+// GetById return a rule based on its ID
+//
+//encore:api public method=GET path=/rule/:id
+func GetById(ctx context.Context, id int) (*Rule, error) {
+	query := `select * from rule
+			where id = $1`
+	row := ruleDb.QueryRow(ctx, query, id)
+	rule := &Rule{}
+	err := row.Scan(&rule.ID, &rule.Name, &rule.Desc, &rule.Salience, &rule.When, &rule.Then)
+	if err != nil {
+		return nil, err
+	}
+	return rule, nil
+}
