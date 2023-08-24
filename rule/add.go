0f7ee9118800e75ce75ee941ad0f5e491719995a
Erickype <erickype@hotmail.com>
2023-08-24 13:21:50 -0500

    Add rule to store the json_data

--- rule/add.go
+++ rule/add.go
@@ -1,14 +1,21 @@
 package rule
 
-import "context"
+import (
+	"context"
+	"encoding/json"
+)
 
 // Add inserts a Rule in the database
 //
 //encore:api public method=POST path=/rule
 func Add(ctx context.Context, rule *Rule) error {
-	query := `insert into rule (name, "desc", salience, "when", "then")
-			values($1, $2, $3, $4, $5)`
-	_, err := ruleDb.Exec(ctx, query, rule.Name, rule.Desc, rule.Salience, rule.When, rule.Then)
+	ruleMarshal, err := json.Marshal(rule)
+	if err != nil {
+		return err
+	}
+	query := `insert into rule (name, "desc", salience, "json_data")
+			values($1, $2, $3, $4)`
+	_, err = ruleDb.Exec(ctx, query, rule.Name, rule.Desc, rule.Salience, ruleMarshal)
 	if err != nil {
 		return err
 	}
