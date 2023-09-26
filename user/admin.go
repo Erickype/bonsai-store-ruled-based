634445df8d758017cbc6c04ae37a9b12f9ba6434
Erick Carrasco <erickype@hotmail.com>
2023-09-26 15:35:06 +0000

    Make admin endpoint

--- user/admin.go
+++ user/admin.go
@@ -3,20 +3,25 @@ package user
 import (
 	"context"
 
+	"encore.dev/beta/errs"
 	"encore.dev/storage/sqldb"
-	"encore.dev/types/uuid"
 )
 
-//encore:api auth method=POST path=/user/admin/:uuid
-func MakeAdmin(context context.Context, uuid uuid.UUID) error {
-	err := CheckUser(context, uuid)
+// MakeAdmin asigns the admin role to a user
+//
+//encore:api auth method=POST path=/user/admin/:email
+func MakeAdmin(context context.Context, email string) error {
+	user, err := fbAuth.GetUserByEmail(context, email)
 	if err != nil {
-		return err
+		return &errs.Error{
+			Code:    errs.NotFound,
+			Message: "User not found",
+		}
 	}
-	query := `update user
+	query := `update "user"
 			  set isAdmin = true
 			  where uuid = $1`
-	_, err = sqldb.Exec(context, query, uuid)
+	_, err = sqldb.Exec(context, query, user.UID)
 	if err != nil {
 		return err
 	}
