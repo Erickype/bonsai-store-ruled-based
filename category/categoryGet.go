3b468932a2fef59ac9c73fb005f4ee91a46247dc
Erick Carrasco <erickype@hotmail.com>
2023-10-14 22:55:16 +0000

    Category service Get endpoint

---
+++ category/categoryGet.go
@@ -0,0 +1,24 @@
+package category
+
+import (
+	"context"
+)
+
+// GetById returns the information of a category based on the id from path
+// returns a Category struct.
+//
+//encore:api public method=GET path=/subcategory/category/:id
+func GetById(ctx context.Context, id int64) (*Category, error) {
+	category := &Category{}
+	query := `select *
+			  from public.category
+			  where id = $1`
+	row := categoryDb.QueryRow(ctx, query, id)
+
+	err := row.Scan(&category.ID, &category.Name, &category.Description)
+	if err != nil {
+		return nil, err
+	}
+
+	return category, nil
+}
