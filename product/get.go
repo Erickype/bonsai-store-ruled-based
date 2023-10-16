3abb4c049a8f0ea0bf60fa4f8d9f29a3396212a3
Erick Carrasco <erickype@hotmail.com>
2023-10-16 22:12:26 +0000

    Products service Get endpoint

--- product/get.go
+++ product/get.go
@@ -17,3 +17,31 @@ func GetByID(ctx context.Context, id int64) (*Product, error) {
 	}
 	return product, nil
 }
+
+// GetResponse is the response of Get method
+type GetResponse struct {
+	// Products an array of products
+	Products []*Product `json:"products"`
+}
+
+// Get returns the list of products
+//
+//encore:api public method=GET path=/product
+func Get(ctx context.Context) (*GetResponse, error) {
+	response := &GetResponse{}
+	query := `select id, subcategory, name, description
+			  from product`
+	rows, err := productDb.Query(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+	for rows.Next() {
+		product := &Product{}
+		err = rows.Scan(&product.ID, &product.Subcategory, &product.Name, &product.Description)
+		if err != nil {
+			return nil, err
+		}
+		response.Products = append(response.Products, product)
+	}
+	return response, nil
+}
