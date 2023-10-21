e264bf3ac5f0fd8d049a46a81aef59c208e0f818
Erick Carrasco <erickype@hotmail.com>
2023-10-21 20:03:21 +0000

    Product service, GetByCategory enpoint

--- product/get.go
+++ product/get.go
@@ -1,6 +1,13 @@
 package product
 
-import "context"
+import (
+	"context"
+	"strconv"
+	"strings"
+
+	"encore.dev/storage/sqldb"
+	"github.com/Erickype/bonsai-store-ruled-based/subcategory"
+)
 
 // GetByID return a Product by a param ID
 //
@@ -28,29 +35,54 @@ type ProductsResponse struct {
 //
 //encore:api public method=GET path=/product
 func Get(ctx context.Context) (*ProductsResponse, error) {
-	response := &ProductsResponse{}
 	query := `select id, subcategory, name, description
 			  from product`
 	rows, err := productDb.Query(ctx, query)
 	if err != nil {
 		return nil, err
 	}
-	for rows.Next() {
-		product := &Product{}
-		err = rows.Scan(&product.ID, &product.Subcategory, &product.Name, &product.Description)
-		if err != nil {
-			return nil, err
-		}
-		response.Products = append(response.Products, product)
-	}
-	return response, nil
+	return processQueryResponse(rows)
 }
 
 // GetByCategory returns the list of products by a category ID passed in path
 //
 //encore:api public method=GET path=/product/category/:categoryID
 func GetByCategory(ctx context.Context, categoryID int64) (*ProductsResponse, error) {
-	response := &ProductsResponse{}
+	subcatResponse, err := subcategory.GetByCategory(ctx, categoryID)
+	if err != nil {
+		return nil, err
+	}
+	var subCatIDs []int64
+	for _, subcategory := range subcatResponse.Subcategories {
+		subCatIDs = append(subCatIDs, subcategory.ID)
+	}
+	// Create a parameterized query with the appropriate number of placeholders
+	query := "SELECT id, subcategory, name, description FROM product WHERE subcategory IN ("
+	placeholders := make([]string, len(subCatIDs))
+	args := make([]interface{}, len(subCatIDs))
 
+	for i := range subCatIDs {
+		placeholders[i] = "$" + strconv.Itoa(i+1)
+		args[i] = subCatIDs[i]
+	}
+	query += strings.Join(placeholders, ", ") + ")"
+	// Now you can execute the query with the IDs
+	rows, err := productDb.Query(ctx, query, args...)
+	if err != nil {
+		return nil, err
+	}
+	return processQueryResponse(rows)
+}
+
+func processQueryResponse(rows *sqldb.Rows) (*ProductsResponse, error) {
+	response := &ProductsResponse{}
+	for rows.Next() {
+		product := &Product{}
+		err := rows.Scan(&product.ID, &product.Subcategory, &product.Name, &product.Description)
+		if err != nil {
+			return nil, err
+		}
+		response.Products = append(response.Products, product)
+	}
 	return response, nil
 }
